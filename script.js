3ac3e77b7f50ae339c82ec2c7395989984110676
Maktum <mhmaktum006@gmail.com>
2025-08-29 20:08:04 +0600

    electricity went out before i lose anything pushing my last update

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 
 const allInfo = [
-    { title: "National Emergency Number", subtitle: "National Emergency", number: "999", tag: "All", iconBg: "#ffe3e2", icon: "./assets/emergency.png" },
-    { title: "Police Help Number", subtitle: "Police", number: "999", tag: "Police", iconBg: "#e2f0ff", icon: "./assets/police.png" },
-    { title: "Fire Service Number", subtitle: "Fire Service", number: "999", tag: "Fire", iconBg: "#ffe3e2", icon: "./assets/fire-service.png" },
-    { title: "Ambulance Service", subtitle: "Ambulance", number: "19994-999999", tag: "Health", iconBg: "#ffe3e2", icon: "./assets/ambulance.png" },
-    { title: "Women & Child Helpline", subtitle: "Women & Child Helpline", number: "109", tag: "Help", iconBg: "#ffe3e2", icon: "./assets/emergency.png" },
-    { title: "Anti-Corruption Helpline", subtitle: "Anti-Corruption", number: "106", tag: "Govt.", iconBg: "#ffe3e2", icon: "./assets/emergency.png" },
-    { title: "Electricity Helpline", subtitle: "Electricity Outage", number: "16216", tag: "Electricity", iconBg: "#ffe3e2", icon: "./assets/emergency.png" },
-    { title: "Brac Helpline", subtitle: "Brac", number: "16445", tag: "NGO", iconBg: "#ffe3e2", icon: "./assets/emergency.png" },
-    { title: "Bangladesh Railway Helpline ", subtitle: "Bangladesh Railway", number: "163", tag: "Travel", iconBg: "#ffe3e2", icon: "./assets/emergency.png" }
+    { title: "National Emergency Number", subtitle: "National Emergency", number: "999", tag: "All",  icon: "./assets/emergency.png" },
+    { title: "Police Help Number", subtitle: "Police", number: "999", tag: "Police", icon: "./assets/police.png" },
+    { title: "Fire Service Number", subtitle: "Fire Service", number: "999", tag: "Fire", icon: "./assets/fire-service.png" },
+    { title: "Ambulance Service", subtitle: "Ambulance", number: "19994-999999", tag: "Health",  icon: "./assets/ambulance.png" },
+    { title: "Women & Child Helpline", subtitle: "Women & Child Helpline", number: "109", tag: "Help", icon: "./assets/emergency.png" },
+    { title: "Anti-Corruption Helpline", subtitle: "Anti-Corruption", number: "106", tag: "Govt.", icon: "./assets/emergency.png" },
+    { title: "Electricity Helpline", subtitle: "Electricity Outage", number: "16216", tag: "Electricity", icon: "./assets/emergency.png" },
+    { title: "Brac Helpline", subtitle: "Brac", number: "16445", tag: "NGO", icon: "./assets/emergency.png" },
+    { title: "Bangladesh Railway Helpline ", subtitle: "Bangladesh Railway", number: "163", tag: "Travel", icon: "./assets/emergency.png" }
 ];
 
 const individualCardInfo = document.getElementById("detail-info");
@@ -19,14 +19,14 @@ for(const data of allInfo){
                 <!-- icon and heart -->
                 <div class="flex justify-between items-center">
                     <div class="flex justify-center items-center size-[60px] bg-[#FFE3E2] rounded-[16px]">
-                        <img class="size-[32px]" src="./assets/emergency.png" alt="">
+                        <img class="size-[32px]" src="${allInfo.icon}">
                     </div>
                     <i class="fa-regular fa-heart fa-xl"></i>
                 </div>
                 <!-- text info -->
                 <div class="pt-[16px]">
-                    <h1 class="font-bold text-2xl">National Emergency Number</h1>
-                    <p class="text-gray-500">National Emergency</p>
+                    <h1 class="font-bold text-2xl">${allInfo.title}</h1>
+                    <p class="text-gray-500">${allInfo.subtitle}</p>
                 </div>
                 <!-- call info -->
                 <div class="pt-[20px] ">
